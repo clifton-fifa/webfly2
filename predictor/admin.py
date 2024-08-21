97337e6f2e606457f14b1bd6eff7f646bc16a738
CliftBlack <thanaponchueboonmee2002@gmail.com>
2024-08-21 22:38:01 +0700

    .

--- predictor/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import YourModel
-
-@admin.register(YourModel)
-class YourModelAdmin(admin.ModelAdmin):
-    list_display = ('field1', 'field2')
--- predictor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('predict_family/', views.predict_family_view, name='predict_family'),
-    path('predict_species/', views.predict_species_view, name='predict_species'),
-]
